fd9d98d5927b7a37509dda26f9e44dcd1423636e
Xavier <xavier.pouyadoux@students.salle.url.edu>
2021-12-28 14:38:39 +0100

    version 2 turn off reboot

--- stopreboot.sh
+++ stopreboot.sh
@@ -7,17 +7,27 @@ echo
 
 echo -e "<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Strict//EN"
         "http://www.w3.org/TR/html4/strict.dtd">
-	<html>
-        	<head>
-               		<title>Turn off / Reboot</title>
-        	</head>
-		<body>
-		<h1>Turn off / Reboot</h1>
-		
-		<a href="reboot.sh">reboot</a><br><br><br>
-		
-		<a href="turnoff.sh">turnoff</a><br><br><br>
-		<a href="menu.sh">RETURN</a>
-		</body>
-	</html>
+        <html>
+                <head>
+                        <title>Turn off / Reboot</title>
+                </head>
+                <body>
+                <h1>Turn off / Reboot</h1>
 "
+                sudo touch /etc/sudoers.d/powercontrol
+                sudo chmod 755 /etc/sudoers.d/powercontrol
+                
+		ls = echo ls /etc/sudoers.d
+		"%users ALL=NOPASSWD: \n
+                %users ALL=NOPASSWD:" >> /etc/sudoers.d/powercontrol
+echo -e "
+
+                <a href="reboot.sh">reboot</a><br><br><br>
+				<p>$ls</p>
+                <a href="turnoff.sh">turnoff</a><br><br><br>
+                <a href="menu.sh">RETURN</a>
+                </body>
+        </html>
+
+"
+
