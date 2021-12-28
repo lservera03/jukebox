5ee9c1d01e2ab5b54ef5f12f91b2704527387a27
Xavier <xavier.pouyadoux@students.salle.url.edu>
2021-12-28 13:41:00 +0100

    script turn off and reboot

---
+++ reboot.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+
+echo Content-type: text/html
+echo
+
+
+sudo shutdown -r now
--- stopreboot.sh
+++ stopreboot.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
 
 
+echo Content-type: text/html
+echo
+
+
+echo -e "<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Strict//EN"
+        "http://www.w3.org/TR/html4/strict.dtd">
+	<html>
+        	<head>
+               		<title>Turn off / Reboot</title>
+        	</head>
+		<body>
+		<h1>Turn off / Reboot</h1>
+		
+		<a href="reboot.sh">reboot</a><br><br><br>
+		
+		<a href="turnoff.sh">turnoff</a><br><br><br>
+		<a href="menu.sh">RETURN</a>
+		</body>
+	</html>
+"
