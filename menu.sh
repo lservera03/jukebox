0ea61876e9bd0397bcaa2075c2e6e48f0968d0ae
Xavier <xavier.pouyadoux@students.salle.url.edu>
2021-12-29 17:29:58 +0100

    Enlaces añadidos

--- menu.sh
+++ menu.sh
@@ -15,9 +15,9 @@ echo -e "<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Strict//EN"
 		<h1>Menu</h1>
 		<a href="process_management.sh">Process management</a><br><br>
 		<a href="monitoring.sh">Monitoring</a><br><br>
-		<a>Turn off/Reload</a><br><br>
+		<a href="stopreboot.sh">Turn off/Reload</a><br><br>
 		<a>Log management</a><br><br>
-		<a>User management</a><br><br>
+		<a href="gestionusuarios.sh">User management</a><br><br>
 		<a href="filtering.sh">Packet Filtering</a><br><br>
 		<a>Programmed task management</a><br><br>
 		<a href="jukebox.sh">Musical Functionalities</a><br><br>
